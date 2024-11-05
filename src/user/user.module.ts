4a9d6c307b997fac7dd9030266f71780f0a4216b
csandrisTurr <csaszarandras@turr.hu>
2024-11-05 09:02:29 +0100

    feat: import mongo user into user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { User } from './user';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from 'src/types/schema/User';
+import { Collections } from 'src/types/Collections';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Collections.Users, schema: UserSchema }])
+  ],
   providers: [User],
   controllers: [UserController]
 })
