84c4c1050fa8adea1407f7168e025108f5a2793c
csandrisTurr <csaszarandras@turr.hu>
2024-11-05 11:13:46 +0100

    feat: registration validation

---
+++ src/user/user.validation.ts
@@ -0,0 +1,12 @@
+import { IsString } from "class-validator";
+
+export class RegisterUserDto {
+    @IsString()
+    email: string;
+
+    @IsString()
+    password: string;
+
+    @IsString()
+    displayName: string;
+}
