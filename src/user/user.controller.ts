d8e11f2b3b93ad476a39c2194cacc5e40e89c4d1
csandrisTurr <csaszarandras@turr.hu>
2024-11-05 11:13:57 +0100

    feat: registration

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './user.validation';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(
+        private readonly userService: UserService,
+    ) {}
+
+    @Post('register')
+    async register(@Body() body: RegisterUserDto) {
+        const dbUser = await this.userService.createUser({
+            displayName: body.displayName,
+            email: body.email,
+            password: body.password
+        });
+
+        // TODO: call login action when register finishes, return JWT
+    }
+}
