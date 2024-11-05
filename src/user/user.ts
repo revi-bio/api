45fae3b7ad6e3471f51857f3103d7a0f5eb4a82b
csandrisTurr <csaszarandras@turr.hu>
2024-11-05 09:02:42 +0100

    feat: start user service

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class User {}
+export class User {
+    constructor(
+        private readonly userModel: Model<User>,
+    ) {}
+
+    async createUser(data: {
+        displayName: string;
+        email: string;
+        password: string;
+    }) {
+        const dbUser = new this.userModel({});
+    }
+}
