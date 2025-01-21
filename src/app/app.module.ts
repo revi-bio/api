52f392b99df8d72360029b3ffb09418c61be35c8
csaszarandras@turr.hu <csaszarandras@turr.hu>
2025-01-21 12:29:57 +0100

    feat: compose & mongo config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from 'src/configuration';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/revi'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
--- src/configuration.ts
+++ src/configuration.ts
@@ -1,5 +1,6 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     jwtSecret: process.env.JWT_SECRET || 'THIS SHOULD BE OVERRIDDEN BY A VALUE IN THE .ENV FILE. ALWAYS OVERRIDE THIS VALUE IN PRODUCTION.',
+    mongoUri: process.env.MONGO_URI || 'mongodb://localhost/revi',
     jwtExpiration: '1d',
 });
