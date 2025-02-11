9158a96093a3cb3b5d6b6309ab007a8f29beb4e6
csandrisTurr <csaszarandras@turr.hu>
2025-02-11 19:30:46 +0100

    feat: better validation for auth

--- src/auth/auth.validation.ts
+++ src/auth/auth.validation.ts
@@ -1,13 +1,20 @@
-import { IsString } from "class-validator";
+import { IsAlphanumeric, IsString, Length, Matches } from "class-validator";
 
 export class RegisterUserDto {
     @IsString()
+    @Length(5, 64)
     email: string;
 
     @IsString()
+    @Length(8, 64)
+    @Matches(/(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[!@#$%^&*(),.?":{}|<>]).{8,}/, {
+        message: 'password must be strong'
+    })
     password: string;
 
     @IsString()
+    @IsAlphanumeric()
+    @Length(1, 32)
     displayName: string;
 }
 
