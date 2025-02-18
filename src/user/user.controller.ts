495d785d865a7ad9e5963954350faa7b00a64a16
csandrisTurr <csaszarandras@turr.hu>
2025-02-18 09:06:19 +0100

    feat: selfInfo route
    MAPI-9

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CurrentUser } from './user.decorator';
+import { JwtData } from 'src/types/JwtData';
 
 @Controller('user')
 export class UserController {
     constructor(
         private readonly userService: UserService,
     ) {}
+
+    @Get('@me')
+    public async selfInfo(@CurrentUser() currentUser: JwtData) {
+        const dbUser = await this.userService.findById(currentUser.id);
+        const { _id, _schemaVersion, password, ...data } = dbUser.toJSON();
+
+        return data;
+    }
+
+    @Get(':username')
+    public async getInfo() {
+
+    }
 }
