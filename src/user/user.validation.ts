1e0ad9b84fdaf3ecca1b644041e6b17579a6a33b
csandrisTurr <csaszarandras@turr.hu>
2025-03-07 09:15:22 +0100

    feat: add pw as required field on pw and email change
    MAPI-18

--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -1,12 +1,5 @@
 import { IsAlphanumeric, IsEmail, IsString, IsStrongPassword, Length, MaxLength } from "class-validator";
 
-export class EmailChangeDto {
-    @IsString()
-    @IsEmail()
-    @Length(5, 64)
-    email: string;
-}
-
 export class ChangeDisplayNameDto {
     @IsString()
     @IsEmail()
@@ -15,7 +8,20 @@ export class ChangeDisplayNameDto {
     displayName: string;
 }
 
+export class EmailChangeDto {
+    @IsString()
+    currentPassword: string;
+
+    @IsString()
+    @IsEmail()
+    @Length(5, 64)
+    email: string;
+}
+
 export class ChangePasswordDto {
+    @IsString()
+    currentPassword: string;
+
     @IsString()
     @MaxLength(64)
     @IsStrongPassword({
