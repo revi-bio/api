2130de2d77e5ae575fd40cd03a1d4e3bbd2cd392
csandrisTurr <csaszarandras@turr.hu>
2025-03-07 10:04:30 +0100

    feat: impl pass and email change properly
    MAPI-18

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Patch, Post, UnauthorizedException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CurrentUser } from './user.decorator';
 import { JwtData } from 'src/types/JwtData';
-import { ChangeDisplayNameDto, EmailChangeDto } from './user.validation';
+import { ChangeDisplayNameDto, ChangePasswordDto, EmailChangeDto } from './user.validation';
+import * as argon2 from 'argon2';
 
 @Controller('user')
 export class UserController {
@@ -24,12 +25,26 @@ export class UserController {
     @Patch('change-email')
     async changeEmail(@Body() body: EmailChangeDto, @CurrentUser() currentUser: JwtData) {
         const dbUser = await this.userService.fromJwtData(currentUser);
+        if (!await argon2.verify(dbUser.password, body.currentPassword))
+            throw new ForbiddenException();
+
         dbUser.email = body.email;
 
         await dbUser.save();
     }
 
-    @Patch('change-name')
+    @Patch('change-password')
+    async changePassword(@Body() body: ChangePasswordDto, @CurrentUser() currentUser: JwtData) {
+        const dbUser = await this.userService.fromJwtData(currentUser);
+        if (!await argon2.verify(dbUser.password, body.currentPassword))
+            throw new ForbiddenException();
+
+        dbUser.password = await argon2.hash(body.password);
+
+        await dbUser.save();
+    }
+
+    @Patch('change-displayname')
     async changeDisplayName(@Body() body: ChangeDisplayNameDto, @CurrentUser() currentUser: JwtData) {
         const dbUser = await this.userService.fromJwtData(currentUser);
         dbUser.displayName = body.displayName;
