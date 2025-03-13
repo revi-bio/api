caa3fdde4f69045dc2c07ae32b35e4932b4d1942
csandrisTurr <csaszarandras@turr.hu>
2025-03-13 13:21:26 +0100

    feat: started setting up file upload
    MAPI-14

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { GridFsStorage } from 'multer-gridfs-storage/lib/gridfs';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+    gridFsStorage: GridFsStorage;
+
+    constructor () {
+        this.gridFsStorage = new GridFsStorage({
+            url: '', // TODO: put mongo uri here
+            file: (req, file) => {
+                return new Promise((resolve, reject) => {
+                    const filename = file.originalname.trim();
+                    const fileInfo = {
+                      filename: filename
+                    };
+                    resolve(fileInfo);
+                });
+              }
+        });
+    }
+
+    createMulterOptions() {
+        return {
+            storage: this.gridFsStorage,
+        };
+    }
+
+    async uploadFile(file: Express.Multer.File, options: any) {
+
+    }
+}
