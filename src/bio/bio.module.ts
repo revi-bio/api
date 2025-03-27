485273dbc6c561fa0ff438a1bbfd745a0d6cff21
csandrisTurr <csaszarandras@turr.hu>
2025-03-27 11:47:48 +0100

    feat: import bio schema into bio module and service
    MAPI-22

--- src/bio/bio.module.ts
+++ src/bio/bio.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BioService } from './bio.service';
 import { BioController } from './bio.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Collections } from 'src/types/Collections';
+import { BioSchema } from 'src/types/schema/Bio';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Collections.Bio, schema: BioSchema },
+    ])
+  ],
   providers: [BioService],
   controllers: [BioController]
 })
-export class BioModule {}
+export class BioModule { }
--- src/bio/bio.service.ts
+++ src/bio/bio.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Collections } from 'src/types/Collections';
+import { Bio } from 'src/types/schema/Bio';
 
 @Injectable()
-export class BioService {}
+export class BioService {
+  constructor(
+    @InjectModel(Collections.Bio) private readonly bioModel: Model<Bio>,
+  ) { }
+
+  async createBio() { }
+}
