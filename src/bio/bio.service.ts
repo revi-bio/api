589ea2ac273ecb87ff8889e3117c4b193aa0328a
csandrisTurr <csaszarandras@turr.hu>
2025-03-27 12:08:55 +0100

    feat: add service method for creating bio sites
    MAPI-22

--- src/bio/bio.service.ts
+++ src/bio/bio.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Collections } from 'src/types/Collections';
-import { Bio } from 'src/types/schema/Bio';
+import { Bio, BioDocument } from 'src/types/schema/Bio';
+import { User } from 'src/types/schema/User';
 
 @Injectable()
 export class BioService {
@@ -10,5 +11,28 @@ export class BioService {
     @InjectModel(Collections.Bio) private readonly bioModel: Model<Bio>,
   ) { }
 
-  async createBio() { }
+  async createBio(data: {
+    handle: string,
+    name: string,
+    user: User,
+  }): Promise<BioDocument> {
+    const { handle, name, user } = data;
+
+    if (await this.bioModel.findOne({ handle }))
+      throw new ConflictException({ message: 'There is already a bio site with this handle' })
+
+    if (await this.bioModel.countDocuments({ user }))
+      throw new BadRequestException({ message: 'Maximum count of bio pages exceeded' })
+
+    const dbBio = new this.bioModel({
+      handle,
+      name,
+      user,
+      widgets: [],
+    });
+
+    await dbBio.save();
+
+    return dbBio;
+  }
 }
