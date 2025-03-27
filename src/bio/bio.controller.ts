ec179b45dbcae14b837edac3ddbbf5594e534ce3
csandrisTurr <csaszarandras@turr.hu>
2025-03-27 12:09:14 +0100

    feat: add empty bio CRUD routes
    MAPI-22

--- src/bio/bio.controller.ts
+++ src/bio/bio.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 
 @Controller('bio')
-export class BioController {}
+export class BioController {
+  @Get()
+  async getBios() {
+
+  }
+
+  @Get()
+  async getBio() {
+
+  }
+
+  @Post()
+  async createBio() {
+
+  }
+
+  @Patch()
+  async editBio() {
+
+  }
+
+  @Delete()
+  async deleteBio() {
+
+  }
+}
